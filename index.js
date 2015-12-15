28df9831c7b96265ed0288021b412210eeb04fdf
Shimon Doodkin <helpmepro1@gmail.com>
2015-12-15 12:03:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ objfilter=function (obj,func)
 //  sort object  by keys
 //  sortobj(obj,function compfunc(a,b,aname,bname)
 //  {
-//            if(aname>bbane) return 1;if(aname<bname) return -1;
+//            if(aname>bname) return 1;if(aname<bname) return -1;
 //            return 0
 //  })
 //
